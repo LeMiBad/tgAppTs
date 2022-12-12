519d837075a9f10eaf23440783154023fa148ebf
Leonid <79872121399.yandex@gmail.com>
2022-12-12 21:46:39 +0300

    first commit

--- src/components/Basket/Basket.tsx
+++ src/components/Basket/Basket.tsx
@@ -97,8 +97,8 @@ const Basket = () => {
             window.Telegram.WebApp.MainButton.show()
             window.Telegram.WebApp.MainButton.setParams({
                 text: `Оформить заказ на ${getSumOfValue()}`,
-                color: dark? '#000000' : '#ffffff',
-                textColor: dark? '#ffffff' : '#000000'
+                color: dark? '#ffffff' : '#000000',
+                textColor: dark? '#000000' : '#ffffff'
             })
         }
         else {
