7fe0dbb133ca11b86680f4f17243daf5cefa55b9
Leonid <79872121399.yandex@gmail.com>
2022-12-13 01:33:15 +0300

    first commit

--- src/components/Basket/Basket.tsx
+++ src/components/Basket/Basket.tsx
@@ -87,7 +87,7 @@ const Basket = () => {
 
     const getSumOfValue = () => {
         return basket.reduce((acc, item) => {
-            return acc + Number(item.price)
+            return acc + +item.price * item.counter
         }, 0)
     }
 
@@ -121,7 +121,7 @@ const Basket = () => {
                                         <h3 style={{marginTop: "10px"}}>{product.name}</h3>
                                         <h4>{product.desk}</h4>
                                     </div>
-                                    <h3 style={{marginBottom: "10px"}}>{product.price}₽</h3>
+                                    <h3 style={{marginBottom: "10px"}}>{+product.price * product.counter}₽</h3>
                                 </StyledBasketProps>
                             </div>
                             <svg onClick={() => {deleteBasketItem(i)}} style={{cursor: 'pointer', width: '20%', margin: 'auto 0'}} focusable="false" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="m11.9997 13.4149 4.9498 4.9497 1.4142-1.4142-4.9498-4.9497 4.9498-4.9498-1.4142-1.4142-4.9498 4.9498L7.05 5.6367 5.6357 7.051l4.9498 4.9498-4.9497 4.9497L7.05 18.3646l4.9497-4.9497z" fill={dark? 'white' : 'black'}></path></svg>
--- src/store/basket.ts
+++ src/store/basket.ts
@@ -2,6 +2,7 @@ import { createEvent, createStore } from "effector";
 
 interface IBasketItem {
     id: number
+    counter: number
     name: string
     desk: string
     price: string
@@ -15,7 +16,33 @@ const initialBasket: IBasketItem[] = []
 export const addBasketItem = createEvent<IBasketItem>()
 export const deleteBasketItem = createEvent<number>()
 export const $basket = createStore(initialBasket)
-    .on(addBasketItem, (state, newBasketItem) => [...state, {...newBasketItem, id: state.length}])
+    .on(addBasketItem, (state, newBasketItem) => {
+        let isHave = false
+        for(let i = 0; i < state.length; i++) {
+            if(`${state[i].name}${state[i].price}` === `${newBasketItem.name}${newBasketItem.price}`) {
+                isHave = true
+            }
+        }
+
+        let newState = []
+
+        if(isHave) {
+            newState = state.map(prod => {
+                if(`${prod.name}${prod.price}` === `${newBasketItem.name}${newBasketItem.price}`) {
+                    prod.counter += 1
+                    return prod
+                }
+                else {
+                    return prod
+                }
+            })
+        }
+        else {
+            newState = [...state, {...newBasketItem, counter: 1}]
+        }
+
+        return newState
+    })
     .on(deleteBasketItem, (state, index) => {
         state.splice(index, 1)
         console.log(state)
