4400c501af619c807bf52431e35faf25a5e91efc
Leonid <79872121399.yandex@gmail.com>
2022-12-14 02:17:23 +0300

    first commit

--- src/store/tgData.ts
+++ src/store/tgData.ts
@@ -11,7 +11,6 @@ const initialTgInfo = {
 
 const config = {
     headers: {
-        "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
     }
 };
